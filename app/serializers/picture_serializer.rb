d452b145b2045b21f9ed1cc5fc88c2b2dd9e47ca
mhassaan <mhassaanchaudhry@gmail.com>
2017-08-10 15:42:39 +0200

    Set base path to fetch images.

--- app/serializers/picture_serializer.rb
+++ app/serializers/picture_serializer.rb
@@ -2,6 +2,6 @@ class PictureSerializer < ActiveModel::Serializer
   attributes :id,:image
 	belongs_to :imageable, polymorphic: true
 	def image
-		Figaro.env.base_url+object.avatar.url
+		Figaro.env.base_url+'/public'+object.avatar.url
 	end
 end
