654043fefcd36977199d77b9f6dc9b2afab386a4
mhassaan <mhassaanchaudhry@gmail.com>
2017-08-26 20:29:28 +0200

    resolve conflicts

--- app/serializers/picture_serializer.rb
+++ app/serializers/picture_serializer.rb
@@ -2,11 +2,6 @@ class PictureSerializer < ActiveModel::Serializer
   attributes :id,:image
 	belongs_to :imageable, polymorphic: true
 	def image
-<<<<<<< HEAD
 		Figaro.env.base_url+'/public'+object.avatar.url
-=======
-		#Rails.root.join('/',object.avatar.url)
-		Figaro.env.base_url+object.avatar.url
->>>>>>> stage
 	end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,22 +1,14 @@
 class UserSerializer < ActiveModel::Serializer
-<<<<<<< HEAD
-  attributes :id, :email, :authentication_token
-  #attribute :authentication_token, if: :authentication_token
-  belongs_to :meta, polymorphic: true
-=======
+
   attributes :id, :email
   attribute :authentication_token, if: :authentication_token
   #belongs_to :meta, polymorphic: true
->>>>>>> stage
+	
 	has_one :picture, as: :imageable,dependent: :destroy
 	has_many :tags, through: :user_taggings
 
   def authentication_token
-<<<<<<< HEAD
-  	object.auth_token
-=======
   	token = (object.auth_token?)? object.auth_token : nil
->>>>>>> stage
   end
 
 end
